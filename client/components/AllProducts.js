763dd231400a8080e3605429bc89061746eaea03
Christa Kaspo <christa.kaspo@gmail.com>
2019-04-18 10:47:38 -0500

    Backend dealing with filtering for inventory.

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -11,14 +11,11 @@ class AllProducts extends React.Component {
   }
   render() {
     const products = this.props.products
-    const filteredProducts = products.filter(product => {
-      return product.inventory > 0
-    })
     return !products.length ? (
       <div>No Candies!</div>
     ) : (
       <div>
-        {filteredProducts.map(product => {
+        {products.map(product => {
           return (
             <div key={product.id}>
               <Link to={`/products/${product.id}`}>

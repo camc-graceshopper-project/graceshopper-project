08c0973cf9ed3914d3727c4f426174578e35a6b1
Christa Kaspo <christa.kaspo@gmail.com>
2019-04-22 10:10:36 -0500

    Can successfully send an email

---
+++ client/components/Emails.js
@@ -0,0 +1,20 @@
+var mailgun = require('mailgun-js')
+var api_key = 'f6db9e78db3473fe581560d3627f7792-3fb021d1-4dd33531'
+var DOMAIN = 'sandbox51ac6892196a4c74839719aab3fc4eed.mailgun.org'
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN})
+
+var data = {
+  from:
+    'Mailgun Sandbox <postmaster@sandbox51ac6892196a4c74839719aab3fc4eed.mailgun.org>',
+  to: 'Christa Kaspo <christa.kaspo@gmail.com>',
+  subject: 'Hello Christa Kaspo',
+  text:
+    'Congratulations Christa Kaspo, you just sent an email with Mailgun!  You are truly awesome!'
+}
+
+mailgun.messages().send(data, function(error, body) {
+  if (error) {
+    console.log(error)
+  }
+  console.log(body)
+})

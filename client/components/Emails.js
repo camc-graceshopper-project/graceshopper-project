88223a28b76326c92f5206623378aa39ae5defc2
Christa Kaspo <christa.kaspo@gmail.com>
2019-04-22 11:36:48 -0500

    edited route. Can't test because I'm not an admin :(

--- client/components/Emails.js
+++ client/components/Emails.js
@@ -3,16 +3,15 @@ var api_key = 'f6db9e78db3473fe581560d3627f7792-3fb021d1-4dd33531'
 var DOMAIN = 'sandbox51ac6892196a4c74839719aab3fc4eed.mailgun.org'
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN})
 
-var data = {
+let email = {
   from:
     'Mailgun Sandbox <postmaster@sandbox51ac6892196a4c74839719aab3fc4eed.mailgun.org>',
-  to: 'Christa Kaspo <christa.kaspo@gmail.com>',
-  subject: 'Hello Christa Kaspo',
-  text:
-    'Congratulations Christa Kaspo, you just sent an email with Mailgun!  You are truly awesome!'
+  to: User.email,
+  subject: `Order has been placed!`,
+  text: `Hello ${User.name}, your order is being processed.`
 }
 
-mailgun.messages().send(data, function(error, body) {
+mailgun.messages().send(email, function(error, body) {
   if (error) {
     console.log(error)
   }
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 const {Order} = require('../db/models')
+const Mailgun = require('mailgun-js')
 const {isAdmin, isAdminOrIsUser} = require('../middleware/auth.middeware')
 module.exports = router
+
+var api_key = 'f6db9e78db3473fe581560d3627f7792-3fb021d1-4dd33531'
+var DOMAIN = 'sandbox51ac6892196a4c74839719aab3fc4eed.mailgun.org'
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN})
 
 router.get('/', isAdmin, async (req, res, next) => {
   try {
@@ -37,14 +42,28 @@ router.put('/:orderId', isAdmin, async (req, res, next) => {
 })
 
 router.get('/status/:status', async (req, res, next) => {
-  try{
+  try {
     const statusOrders = await Order.findAll({
       where: {
-        status: req.params.status
+        status: req.params.status,
+        userid: req.user.id
       }
     })
-    res.json(statusOrders)
-  }catch(error){
+    let email = {
+      from:
+        'Mailgun Sandbox <postmaster@sandbox51ac6892196a4c74839719aab3fc4eed.mailgun.org>',
+      to: req.user.email,
+      subject: `${req.params.status}`,
+      text: `Hello ${req.user.name}, your order is ${req.params.status}.`
+    }
+    mailgun.messages().send(email, function(error, body) {
+      if (error) {
+        console.log(error)
+      }
+      console.log(body)
+      res.json(statusOrders)
+    })
+  } catch (error) {
     next(error)
   }
 })
